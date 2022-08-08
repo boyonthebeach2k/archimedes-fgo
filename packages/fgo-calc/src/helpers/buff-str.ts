e8b9d8a446b2c7e0f097dfaa9875c7d473b9d237
Boy on the Beach <39589879+boyonthebeach2k@users.noreply.github.com>
2022-08-09 01:08:48 +0530

    Fix accessing empty string in buffTypeToBuffStr

--- packages/fgo-calc/src/helpers/buff-str.ts
+++ packages/fgo-calc/src/helpers/buff-str.ts
@@ -1,61 +1,61 @@
 import { Buff, Card } from "@atlasacademy/api-connector";
 
-const { BuffType: bt } = Buff;
+const { BuffType } = Buff;
 
 const buffTypeToBuffStr = (buffType: Buff.BuffType, value: number, cardType?: Card) => {
-    if (buffType === bt.UP_COMMANDATK) {
-        return `${(cardType ?? "")[0].toLowerCase()}cp${value / 10}`;
+    if (buffType === BuffType.UP_COMMANDATK) {
+        return `${(cardType ?? " ")[0].toLowerCase()}cp${value / 10}`;
     }
-    if (buffType === bt.DOWN_COMMANDATK) {
-        return `${(cardType ?? "")[0].toLowerCase()}cp-${value / 10}`;
+    if (buffType === BuffType.DOWN_COMMANDATK) {
+        return `${(cardType ?? " ")[0].toLowerCase()}cp-${value / 10}`;
     }
-    if (buffType === bt.UP_COMMANDNP) {
+    if (buffType === BuffType.UP_COMMANDNP) {
         return "";
     }
-    if (buffType === bt.DOWN_COMMANDNP) {
+    if (buffType === BuffType.DOWN_COMMANDNP) {
         return "";
     }
-    if (buffType === bt.UP_COMMANDSTAR) {
+    if (buffType === BuffType.UP_COMMANDSTAR) {
         return "";
     }
-    if (buffType === bt.DOWN_COMMANSTAR) {
+    if (buffType === BuffType.DOWN_COMMANSTAR) {
         return "";
     }
-    if (buffType === bt.UP_COMMANDALL) {
-        return `${(cardType ?? "")[0].toLowerCase()}m${value / 10}`;
+    if (buffType === BuffType.UP_COMMANDALL) {
+        return `${(cardType ?? " ")[0].toLowerCase()}m${value / 10}`;
     }
-    if (buffType === bt.DOWN_COMMANDALL) {
-        return `${(cardType ?? "")[0].toLowerCase()}m-${value / 10}`;
+    if (buffType === BuffType.DOWN_COMMANDALL) {
+        return `${(cardType ?? " ")[0].toLowerCase()}m-${value / 10}`;
     }
-    if (buffType === bt.UP_CRITICALDAMAGE) {
-        return `${(cardType ?? "")[0].toLowerCase()}cd${value / 10}`;
+    if (buffType === BuffType.UP_CRITICALDAMAGE) {
+        return `${(cardType ?? " ")[0].toLowerCase()}cd${value / 10}`;
     }
-    if (buffType === bt.DOWN_CRITICALDAMAGE) {
-        return `${(cardType ?? "")[0].toLowerCase()}cd-${value / 10}`;
+    if (buffType === BuffType.DOWN_CRITICALDAMAGE) {
+        return `${(cardType ?? " ")[0].toLowerCase()}cd-${value / 10}`;
     }
 
     return (
         (
             {
-                [bt.UP_ATK]: `a${value / 10}`,
-                [bt.DOWN_ATK]: `a-${value / 10}`,
-                [bt.UP_CRITICALPOINT]: `sg${value / 10}`,
-                [bt.DOWN_CRITICALPOINT]: `sg-${value / 10}`,
-                [bt.UP_DAMAGE]: `p${value / 10}`,
-                [bt.DOWN_DAMAGE]: `p-${value / 10}`,
-                [bt.ADD_DAMAGE]: `fd${value}`,
-                [bt.SUB_DAMAGE]: `fd-${value}`,
-                [bt.UP_NPDAMAGE]: `n${value / 10}`,
-                [bt.DOWN_NPDAMAGE]: `n-${value / 10}`,
-                [bt.UP_DROPNP]: `ng${value / 10}`,
-                [bt.DOWN_DROPNP]: `ng-${value / 10}`,
-                [bt.UP_DEFENCE]: `d${value / 10}`,
-                [bt.DOWN_DEFENCE]: `d-${value / 10}`,
-                [bt.UP_SPECIALDEFENCE]: `sdm${value / 10}`,
-                [bt.DOWN_SPECIALDEFENCE]: `sdm-${value / 10}`,
-                [bt.UP_DAMAGE_SPECIAL]: `sam${value / 10}`,
-            } as Partial<typeof bt>
-        )[buffType as unknown as keyof Partial<typeof bt>] ?? ""
+                [BuffType.UP_ATK]: `a${value / 10}`,
+                [BuffType.DOWN_ATK]: `a-${value / 10}`,
+                [BuffType.UP_CRITICALPOINT]: `sg${value / 10}`,
+                [BuffType.DOWN_CRITICALPOINT]: `sg-${value / 10}`,
+                [BuffType.UP_DAMAGE]: `p${value / 10}`,
+                [BuffType.DOWN_DAMAGE]: `p-${value / 10}`,
+                [BuffType.ADD_DAMAGE]: `fd${value}`,
+                [BuffType.SUB_DAMAGE]: `fd-${value}`,
+                [BuffType.UP_NPDAMAGE]: `n${value / 10}`,
+                [BuffType.DOWN_NPDAMAGE]: `n-${value / 10}`,
+                [BuffType.UP_DROPNP]: `ng${value / 10}`,
+                [BuffType.DOWN_DROPNP]: `ng-${value / 10}`,
+                [BuffType.UP_DEFENCE]: `d${value / 10}`,
+                [BuffType.DOWN_DEFENCE]: `d-${value / 10}`,
+                [BuffType.UP_SPECIALDEFENCE]: `sdm${value / 10}`,
+                [BuffType.DOWN_SPECIALDEFENCE]: `sdm-${value / 10}`,
+                [BuffType.UP_DAMAGE_SPECIAL]: `sam${value / 10}`,
+            } as Partial<typeof BuffType>
+        )[buffType as unknown as keyof Partial<typeof BuffType>] ?? ""
     );
 };
 

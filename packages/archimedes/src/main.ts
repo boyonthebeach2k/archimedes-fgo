7e54e534090926587e949e2be7ae0368ba942d14
Boy on the Beach <39589879+boyonthebeach2k@users.noreply.github.com>
2022-08-16 12:02:47 +0530

    Add rudimentary catch clause to main/init

--- packages/archimedes/src/main.ts
+++ packages/archimedes/src/main.ts
@@ -1,5 +1,6 @@
 import discord from "discord.js";
 import { config as envConfig } from "dotenv";
+import fs from "fs";
 
 import { messageCreateHandler } from "./commands/handlers";
 import { init } from "./helpers/svt";
@@ -9,15 +10,21 @@ envConfig();
 const client = new discord.Client({ intents: 32265, partials: ["CHANNEL", "MESSAGE"] }); //[Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.DIRECT_MESSAGES] });
 const TOKEN = process.argv.map((arg) => arg.toLowerCase()).includes("dev") ? process.env.DEV_TOKEN : process.env.BOT_TOKEN;
 
-init().then(() => {
-    client.on("ready", () => {
-        console.info(`Logged in as ${client.user?.tag}!`);
-        client.user?.setActivity("you", {
-            type: "WATCHING",
+init()
+    .then(() => {
+        client.on("ready", () => {
+            console.info(`Logged in as ${client.user?.tag}!`);
+            client.user?.setActivity("you", {
+                type: "WATCHING",
+            });
         });
+
+        client.on("messageCreate", messageCreateHandler);
+
+        client.login(TOKEN);
+    })
+    .catch((err) => {
+        console.error(err);
+        fs.unlinkSync("./assets/api-info.json");
+        process.exit(10); // ERROR
     });
-
-    client.on("messageCreate", messageCreateHandler);
-
-    client.login(TOKEN);
-});

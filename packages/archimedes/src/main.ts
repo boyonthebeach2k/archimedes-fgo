2e325df3cb0723b67a3b63847a6b8b6844004c29
Boy on the Beach <39589879+boyonthebeach2k@users.noreply.github.com>
2022-08-25 18:05:00 +0530

    Override console loggers with jury-rigged logger

--- packages/archimedes/src/main.ts
+++ packages/archimedes/src/main.ts
@@ -9,10 +9,45 @@ envConfig();
 const client = new discord.Client({ intents: 32265, partials: ["CHANNEL", "MESSAGE"] }); //[Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.DIRECT_MESSAGES] });
 const TOKEN = process.argv.map((arg) => arg.toLowerCase()).includes("dev") ? process.env.DEV_TOKEN : process.env.BOT_TOKEN;
 
+const readyLogs = () => {
+    const levels = {
+        alert: 1,
+        error: 3,
+        warn: 4,
+        info: 6,
+        log: 7,
+    };
+
+    Object.defineProperty(console, "_log", console.log);
+
+    for (const [level, priority] of Object.entries(levels)) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (console as any)[level] = (...args: any[]) => {
+            const line = [`<${priority}>`];
+
+            args.map((arg) => {
+                if (typeof arg !== "string") {
+                    try {
+                        arg = JSON.stringify(arg);
+                    } catch (err) {
+                        arg = arg.toString ? arg.toString : arg;
+                    }
+                }
+
+                line.push(arg);
+            });
+
+            process.stdout.write(`${line.join(" ")}\n`);
+        };
+    }
+};
+
+readyLogs();
+
 init()
     .then(() => {
         client.on("ready", () => {
-            console.info(`Logged in as ${client.user?.tag}!`);
+            console.log(`Logged in as ${client.user?.tag}!`);
             client.user?.setActivity("you", {
                 type: "WATCHING",
             });

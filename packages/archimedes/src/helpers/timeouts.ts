dc4728007a9f2d47a8387d01778ae6f93d6df923
Boy on the Beach <39589879+boyonthebeach2k@users.noreply.github.com>
2023-09-12 19:58:51 +0530

    Track timeouts and intervals created

---
+++ packages/archimedes/src/helpers/timeouts.ts
@@ -0,0 +1,65 @@
+/** Tracks timeouts */
+const timeoutIDs: NodeJS.Timeout[] = [],
+    intervalIDs: NodeJS.Timer[] = [];
+
+/**
+ * Wrapper around setTimeout to keep track of queued functions. Calls `setTimeout` internally,
+ * but keeps track of the returned `timeoutID` for easy clearing afterwards. */
+function scheduleTimeout(callback: CallableFunction, milliseconds: number) {
+    let timeoutID: NodeJS.Timeout;
+
+    const wrappedCallback = () => {
+        callback();
+
+        /** Cleanup - remove the timeoutID from `timeoutIDs` */
+        const timeoutIndex = timeoutIDs.indexOf(timeoutID);
+
+        if (timeoutIndex > -1) {
+            // Only splice timeoutIDs when item is found
+            timeoutIDs.splice(timeoutIndex, 1); // Remove only the one ID
+        }
+    };
+
+    timeoutIDs.push(setTimeout(wrappedCallback, milliseconds));
+}
+
+/**
+ * Clears all queued timeouts
+ */
+function clearTimeouts() {
+    for (const timeoutID of timeoutIDs) {
+        clearTimeout(timeoutID); //  Passing an invalid ID to `clearTimeout()` silently does nothing; no exception is thrown.
+    }
+}
+
+/**
+ * Wrapper around setInterval to keep track of queued functions. Calls `setInterval` internally,
+ * but keeps track of the returned `intervalID` for easy clearing afterwards. */
+function scheduleInterval(callback: CallableFunction, milliseconds: number) {
+    let intervalID: NodeJS.Timer;
+
+    const wrappedCallback = () => {
+        callback();
+
+        /** Cleanup - remove the intervalID from `intervalIDs` */
+        const intervalIndex = intervalIDs.indexOf(intervalID);
+
+        if (intervalIndex > -1) {
+            // Only splice intervalIDs when item is found
+            intervalIDs.splice(intervalIndex, 1); // Remove only the one ID
+        }
+    };
+
+    timeoutIDs.push(setInterval(wrappedCallback, milliseconds));
+}
+
+/**
+ * Clears all queued intervals
+ */
+function clearIntervals() {
+    for (const intervalID of intervalIDs) {
+        clearInterval(intervalID); //  Passing an invalid ID to `clearInterval()` silently does nothing; no exception is thrown.
+    }
+}
+
+export { scheduleTimeout, clearTimeouts, scheduleInterval, clearIntervals };

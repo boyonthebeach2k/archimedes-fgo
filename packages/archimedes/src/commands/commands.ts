06cd522c89976ea7ee0ef02190153591af98b7b3
Archimedes-FGO <106027114+Archimedes-FGO@users.noreply.github.com>
2024-08-07 15:53:13 +0000

    Revert "Remove KR from APK command"
    This reverts commit 885e5c9a77b6c79e0964a6945f7ef99cbf2e66ae.

--- packages/archimedes/src/commands/commands.ts
+++ packages/archimedes/src/commands/commands.ts
@@ -1115,7 +1115,7 @@ async function calc(expr: string) {
 const apkLinkEmbed = async function (
     this: {
         versions: {
-            [key in "CN" | "TW" | "NA 32-bit" | "NA 64-bit"]: { link: string; version: string };
+            [key in "CN" | "TW" | "KR" | "NA 32-bit" | "NA 64-bit"]: { link: string; version: string };
         };
     },
     _: string,
@@ -1155,6 +1155,7 @@ const apkLinkEmbed = async function (
 
     const fetchLinks = async () => {
         const regionMapObject = {
+            KR: "KR",
             TW: "TW",
             NA: "NA 64-bit",
             NA_32: "NA 32-bit",
@@ -1169,7 +1170,7 @@ const apkLinkEmbed = async function (
             };
         }
 
-        const shouldFetchRegions = (["TW", "NA 64-bit", "NA 32-bit"] as const).some(
+        const shouldFetchRegions = (["KR", "TW", "NA 64-bit", "NA 32-bit"] as const).some(
             (region) => this.versions[region].version !== versionListRemote[region].version,
             this
         );
@@ -1179,10 +1180,11 @@ const apkLinkEmbed = async function (
 
             const packages = [
                 { region: "NA", packageId: "com.aniplex.fategrandorder.en" },
+                { region: "KR", packageId: "com.netmarble.fgok" },
                 { region: "TW", packageId: "com.xiaomeng.fategrandorder" },
             ] as const;
 
-            console.debug("Fetching TW/NA apks links...");
+            console.debug("Fetching TW/KR/NA apks links...");
             const versions = await Promise.all(
                 packages.map(async (apk) => (await fetch(`https://gplay-ver.atlasacademy.workers.dev/?id=${apk.packageId}`)).text())
             );
@@ -1259,6 +1261,7 @@ const apkLinkEmbed = async function (
     versions: {
         CN: { link: "", version: "" },
         TW: { link: "", version: "" },
+        KR: { link: "", version: "" },
         "NA 32-bit": { link: "", version: "" },
         "NA 64-bit": { link: "", version: "" },
     },
